dde9689221500261eda7e7c7b869d09ce7cc7e1d
docia <matiushenko107@gmail.com>
2024-06-07 00:35:00 +0300

    installed gsap library

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,80 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 function About() {
+	const sectionRef = useRef();
+
+	useGSAP(
+		() => {
+			const animateFrom = (elem, direction = 1) => {
+				let x = 0;
+				let y = direction * 100;
+				if (elem.classList.contains("gs_reveal_fromLeft")) {
+					x = -100;
+					y = 0;
+				} else if (elem.classList.contains("gs_reveal_fromRight")) {
+					x = 100;
+					y = 0;
+				}
+				elem.style.transform = `translate(${x}px, ${y}px)`;
+				elem.style.opacity = "0";
+				gsap.fromTo(
+					elem,
+					{ x, y, autoAlpha: 0 },
+					{
+						duration: 1.25,
+						x: 0,
+						y: 0,
+						autoAlpha: 1,
+						ease: "expo",
+						overwrite: "auto",
+					}
+				);
+			};
+			const hide = (elem) => {
+				gsap.set(elem, { autoAlpha: 0 });
+			};
+
+			const elements = gsap.utils.toArray(
+				sectionRef.current.querySelectorAll(".gs_reveal")
+			);
+
+			elements.forEach((elem) => {
+				hide(elem); 
+
+				ScrollTrigger.create({
+					trigger: elem,
+					onEnter: () => {
+						animateFrom(elem);
+					},
+					onEnterBack: () => {
+						animateFrom(elem, -1);
+					},
+					onLeave: () => {
+						hide(elem);
+					},
+				});
+			});
+
+			return () => {
+				ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+			};
+		},
+		{ scope: sectionRef }
+	);
+
 	return (
 		<section
 			id="about-me"
 			className="flex flex-col items-center justify-center min-h-screen py-16"
+			ref={sectionRef}
 		>
 			<div className="container px-14 grid md:grid-cols-3 gap-8 items-center max-w-screen-lg ">
-				<div className="avatar flex justify-center items-center md:px-0">
+				<div className="avatar flex justify-center items-center md:px-0 gs_reveal gs_reveal_fromLeft">
 					<img
 						className="rounded-full w-60 h-30"
 						alt="my avatar"
@@ -16,8 +85,8 @@ function About() {
 					id="introduction"
 					className="md:col-span-2 flex flex-col space-y-12 items-center md:items-start max-w-prose"
 				>
-					<h2 className="font-ubuntu text-xl ">About Me</h2>
-					<p className="text-left font-nanum max-w-lg">
+					<h2 className="font-ubuntu text-xl  gs_reveal">About Me</h2>
+					<p className="text-left font-nanum max-w-lg  gs_reveal">
 						Hi! I'm frontend developer. "Lorem ipsum dolor sit amet, consectetur
 						adipiscing elit, sed do eiusmod tempor incididunt ut labore et
 						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,26 @@
 import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(useGSAP);
 
 function Header() {
 	const myRef = useRef(null);
+	const container = useRef(null);
+
+	useGSAP(
+		() => {
+			gsap.to("h1", { duration: 2, text: "Anastasiia Matiushenko" });
+
+			const tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
+			tl.to("p", { duration: 3, text: "...my interactive resume" }, "+=2")
+				.to("p", { duration: 1, opacity: 0 })
+		},
+		{ scope: container }
+	);
 
 	useEffect(() => {
 		const handleScroll = () => {
@@ -19,9 +38,12 @@ function Header() {
 	return (
 		<section ref={myRef} id="header" className="relative flex h-screen w-full">
 			<div className="absolute inset-0 bg-cover bg-fixed bg-center bg-no-repeat bg-galaxy opacity-50"></div>
-			<div className="relative flex-1 flex items-center flex-col justify-center text-xl">
-				<h1 className="opacity-100 font-ubuntu text-5xl">Anastasiia Matiushenko</h1>
-				<p className="opacity-100 font-nanum">...my interactive resume</p>
+			<div
+				ref={container}
+				className="relative flex-1 flex items-center flex-col justify-center text-xl"
+			>
+				<h1 className="opacity-100 font-ubuntu text-5xl"></h1>
+				<p className="opacity-100 font-nanum"></p>
 			</div>
 		</section>
 	);
